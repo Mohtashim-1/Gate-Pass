cc4c7c6cacbcc65cf842e3a0eb691f02a1832c3d
Mohtashim <shoaibmohtashim973@gmail.com>
2024-05-28 08:16:32 +0000

    testing

--- gate_pass/gate_pass/doctype/address/address.py
+++ gate_pass/gate_pass/doctype/address/address.py
@@ -4,5 +4,9 @@
 import frappe
 from frappe.model.document import Document
 
-class CourierGatePass(Document):
-	pass
+class Address(Document):
+	def before_save(self):
+		self.set_full_address()
+	
+	def set_full_address(self):
+		self.full_address = "abcd"
