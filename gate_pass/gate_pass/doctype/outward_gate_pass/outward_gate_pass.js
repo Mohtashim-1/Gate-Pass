f386c5b5ddcb407593e32e9924b69c230f176bba
Mohtashim <shoaibmohtashim973@gmail.com>
2024-05-28 22:02:45 +0000

    server scripts

--- gate_pass/gate_pass/doctype/outward_gate_pass/outward_gate_pass.js
+++ gate_pass/gate_pass/doctype/outward_gate_pass/outward_gate_pass.js
@@ -2,7 +2,37 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Outward Gate Pass', {
-	// refresh: function(frm) {
+	refresh(frm) {
+		// your code here
+	}
+})
 
-	// }
+
+frappe.ui.form.on('Outward Gate Pass', {
+	refresh: function (frm) {
+		// Modify the get_query_filters function
+		frm.fields_dict['customer'].get_query = function (doc) {
+			return {
+				filters: {
+					'disabled': 0 // Only fetch customers with 'disabled' set to 0 (not disabled)
+				}
+			};
+		};
+	}
 });
+
+
+frappe.ui.form.on('Outward Gate Pass', {
+	refresh: function (frm) {
+		// Modify the get_query_filters function
+		frm.fields_dict['supplier'].get_query = function (doc) {
+			return {
+				filters: {
+					'disabled': 0 // Only fetch customers with 'disabled' set to 0 (not disabled)
+				}
+			};
+		};
+	}
+});
+
+
