f9d2e7c41d6d2207fe5a488e7eebcc4696859681
Mohtashim <shoaibmohtashim973@gmail.com>
2024-05-28 10:05:07 +0000

    setting of address

--- gate_pass/gate_pass/doctype/address/address.py
+++ gate_pass/gate_pass/doctype/address/address.py
@@ -5,8 +5,12 @@ import frappe
 from frappe.model.document import Document
 
 class Address(Document):
-	def before_save(self):
+	def validate(self):
 		self.set_full_address()
+		frappe.msgprint('Message')
+		
 	
 	def set_full_address(self):
 		self.full_address = "abcd"
+		frappe.msgprint('Message')
+		
