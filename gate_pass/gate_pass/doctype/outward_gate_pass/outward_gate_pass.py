cd3182d736854f1b5f1bf01073379959f135536e
Mohtashim-1 <shoaibmohtashim973@gmail.com>
2024-06-14 12:06:35 +0000

    fix

--- gate_pass/gate_pass/doctype/outward_gate_pass/outward_gate_pass.py
+++ gate_pass/gate_pass/doctype/outward_gate_pass/outward_gate_pass.py
@@ -1,8 +1,57 @@
-# Copyright (c) 2024, Mohtashim and contributors
-# For license information, please see license.txt
-
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class OutwardGatePass(Document):
-	pass
+    def before_save(self):
+        self.send_outward_gate_to_stock_entry()
+
+    def send_outward_gate_to_stock_entry(self):
+        gate_pass = self
+
+        if gate_pass.ogp_type == "Inventory" and gate_pass.type == "Returnable":
+            stock_entry_items = []
+            for item in gate_pass.inventory_gate_pass:
+                stock_entry_items.append({
+                    'item_code': item.item,
+                    'qty': item.qty,
+                    's_warehouse': item.s_warehouse,
+                    't_warehouse': item.t_warehouse
+                })
+            
+            stock_entry = frappe.get_doc({
+                'doctype': 'Stock Entry',
+                'purpose': 'Material Transfer',
+                'posting_date': gate_pass.posting_date,  # Ensure posting_date exists
+                'stock_entry_type': 'Material Transfer',
+                'items': stock_entry_items
+            })
+            
+            stock_entry.insert(ignore_permissions=True)
+            stock_entry.save()
+            # stock_entry.submit()
+            
+            frappe.msgprint(f"Stock Entry has been created: {stock_entry.name}")
+        
+        elif gate_pass.ogp_type == "Inventory" and gate_pass.type == "Non-Returnable":
+            stock_entry_items = []
+            for item in gate_pass.inventory_gate_pass:
+                stock_entry_items.append({
+                    'item_code': item.item,
+                    'qty': item.qty,
+                    's_warehouse': item.s_warehouse,
+                    # 't_warehouse': item.t_warehouse
+                })
+                
+            stock_entry = frappe.get_doc({
+                'doctype': 'Stock Entry',
+                'purpose': 'Material Issue',
+                'posting_date': gate_pass.creation_date,  # Ensure posting_date exists
+                'stock_entry_type': 'Material Issue',
+                'items': stock_entry_items
+            })
+            
+            stock_entry.insert(ignore_permissions=True)
+            stock_entry.save()
+            # stock_entry.submit()
+            
+            frappe.msgprint(f"Stock Entry has been created: {stock_entry.name}")
