4e85a03ade7ac6f16e4a671376a6bdcc5317df3e
Mohtashim-1 <shoaibmohtashim973@gmail.com>
2024-06-21 06:43:29 +0000

    fix

--- gate_pass/gate_pass/doctype/outward_gate_pass/outward_gate_pass.py
+++ gate_pass/gate_pass/doctype/outward_gate_pass/outward_gate_pass.py
@@ -36,10 +36,11 @@ class OutwardGatePass(Document):
 		# frappe.msgprint("a")
   
     def send_outward_gate_to_stock_entry(self):
+        
         gate_pass = self
 
         # if gate_pass.ogp_type == "Inventory" and gate_pass.type == "Returnable" and gate_pass.workflow_state == "For Approval":
-        if gate_pass.ogp_type == "Inventory" and gate_pass.type == "Returnable" and gate_pass.workflow_state == "Direct Approved" or gate_pass.workflow_state == "For Approval" :
+        if gate_pass.ogp_type == "Inventory" and gate_pass.type == "Returnable" and gate_pass.workflow_state == "For Approval":
             stock_entry_items = []
             frappe.errprint(gate_pass.workflow_state)
             for item in gate_pass.inventory_gate_pass:
@@ -61,29 +62,52 @@ class OutwardGatePass(Document):
             stock_entry.insert(ignore_permissions=True)
             stock_entry.save()
             # stock_entry.submit()
-            
-            frappe.msgprint(f"Stock Entry has been created: {stock_entry.name}")
-        
-        elif gate_pass.ogp_type == "Inventory" and gate_pass.type == "Non-Returnable" and gate_pass.workflow_state == "Approved":
-            stock_entry_items = []
-            for item in gate_pass.inventory_gate_pass:
-                stock_entry_items.append({
-                    'item_code': item.item,
-                    'qty': item.qty,
-                    's_warehouse': item.s_warehouse,
-                    't_warehouse': item.t_warehouse
+
+            if gate_pass.ogp_type == "Inventory" and gate_pass.type == "Returnable" and gate_pass.workflow_state == "Direct Approved":
+                stock_entry_items = []
+                frappe.errprint(gate_pass.workflow_state)
+                for item in gate_pass.inventory_gate_pass:
+                    stock_entry_items.append({
+                        'item_code': item.item,
+                        'qty': item.qty,
+                        's_warehouse': item.s_warehouse,
+                        't_warehouse': item.t_warehouse
+                    })
+                
+                stock_entry = frappe.get_doc({
+                    'doctype': 'Stock Entry',
+                    'purpose': 'Material Transfer',
+                    'posting_date': gate_pass.creation_date,
+                    'stock_entry_type': 'Material Transfer',
+                    'items': stock_entry_items
                 })
                 
-            stock_entry = frappe.get_doc({
-                'doctype': 'Stock Entry',
-                'purpose': 'Material Issue',
-                'posting_date': gate_pass.creation_date,
-                'stock_entry_type': 'Material Issue',
-                'items': stock_entry_items
-            })
-            
-            stock_entry.insert(ignore_permissions=True)
-            stock_entry.save()
+                stock_entry.insert(ignore_permissions=True)
+                stock_entry.save()
             # stock_entry.submit()
             
             frappe.msgprint(f"Stock Entry has been created: {stock_entry.name}")
+        
+        # elif gate_pass.ogp_type == "Inventory" and gate_pass.type == "Non-Returnable" and gate_pass.workflow_state == "Approved":
+        #     stock_entry_items = []
+        #     for item in gate_pass.inventory_gate_pass:
+        #         stock_entry_items.append({
+        #             'item_code': item.item,
+        #             'qty': item.qty,
+        #             's_warehouse': item.s_warehouse,
+        #             't_warehouse': item.t_warehouse
+        #         })
+                
+        #     stock_entry = frappe.get_doc({
+        #         'doctype': 'Stock Entry',
+        #         'purpose': 'Material Issue',
+        #         'posting_date': gate_pass.creation_date,
+        #         'stock_entry_type': 'Material Issue',
+        #         'items': stock_entry_items
+        #     })
+            
+        #     stock_entry.insert(ignore_permissions=True)
+        #     stock_entry.save()
+        #     # stock_entry.submit()
+            
+        #     frappe.msgprint(f"Stock Entry has been created: {stock_entry.name}")
